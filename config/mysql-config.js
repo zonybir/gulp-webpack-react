1fbfa4412f0e16a91a2b50c7168cbc7e1bb45e7c
zonybir-u <zony_zhang@uyixing.com>
2015-11-28 18:02:17 +0800

    change mysql passwrod

--- config/mysql-config.js
+++ config/mysql-config.js
@@ -1,7 +1,7 @@
 module.exports={
-	host       : '127.0.0.1',
-    user       : 'root',
-    password   : 'zony2015',
-    database   : 'zonybir',
-    prefix     : 'enlink_'
+    host              : '127.0.0.1',
+    user              : 'root',
+    password   : 'root',
+    database    : 'zonybir',
+    prefix           : 'enlink_'
 }
